061a5044111a841190e0ef33384f975bd58d1ea6
Горчаков Дмитрий <d.gorchakov@hh.ru>
2023-03-17 22:41:36 +0300

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const POPUP_OPEN = 'popup_open';
+const POPUP_OPEN = 'popup_opened';
 let editButton = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-btn')
 let name = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__about');
 let popup = document.querySelector('.popup');
-let editForm = popup.querySelector('.edit-profile');
-let formNameInput = editForm.querySelector('.edit-profile__text-input_name');
-let formAboutInput = editForm.querySelector('.edit-profile__text-input_about');
+let editForm = popup.querySelector('.form');
+let formNameInput = editForm.querySelector('.form__input_type_name');
+let formAboutInput = editForm.querySelector('.form__input_type_about');
 
 editButton.addEventListener('click', ()=> {
     formNameInput.value = name.innerHTML;
