988046c43f205c60e9330e172b95bb1097e6c5e6
DGorchakov <71970557+DGorchakov@users.noreply.github.com>
2023-05-21 00:10:56 +0300

    Revert (#2)
    * Revert "I did that"
    
    This reverts commit 44b52aa425c51b4dd35f42b196f5dddee13e88ad.
    
    * Revert "revert"
    
    This reverts commit 18165b79c19027ea1018bb8666da1cef45c0a72c.
    
    ---------
    
    Co-authored-by: Горчаков Дмитрий <d.gorchakov@hh.ru>

---
+++ scripts/FormValidator.js
@@ -0,0 +1,77 @@
+export default class FormValidator {
+
+    _submitButton;
+    _inputList;
+    _inactiveButtonClass;
+
+    constructor(formSelectors, formElement) {
+        this._formSelectors = formSelectors;
+        this._formElement = formElement;
+        this._inputList = Array.from(formElement.querySelectorAll(formSelectors.inputSelector));
+        this._submitButton = formElement.querySelector(formSelectors.submitButtonSelector);
+        this._inactiveButtonClass = formSelectors.inactiveButtonClass;
+    }
+
+    enableValidation() {
+        this._formElement.addEventListener('submit', e => {
+                e.preventDefault();
+        });
+        this._setEventListeners();
+    };
+
+    _setEventListeners() {
+        this._toggleButtonState();
+        this._inputList.forEach(inputEl => {
+            inputEl.addEventListener('input', () => {
+                this._checkInputValidity(inputEl);
+                this._toggleButtonState();
+            });
+        });
+        this._formElement.addEventListener('reset', () => {
+            this._disableSubmitButton(true);
+        })
+    }
+    
+    _toggleButtonState() {
+        this._hasInvalidInput() ? this._disableSubmitButton(true) 
+                                : this._disableSubmitButton(false);
+    }
+    
+    _hasInvalidInput() {
+        return this._inputList.some(e => !e.validity.valid);
+    }
+    
+    _checkInputValidity(inputElement) {
+        if (!inputElement.validity.valid) {
+          this._showInputError(inputElement, inputElement.validationMessage);
+        } else {
+          this._hideInputError(inputElement);
+        }
+    }
+    
+    _showInputError(inputElement, errorMessage) {
+        const errorClass= this._formSelectors.errorClass;
+        const inputErrorClass= this._formSelectors.inputErrorClass;
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+
+        inputElement.classList.add(inputErrorClass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(errorClass);
+      }
+      
+    _hideInputError(inputElement) {
+        const errorClass= this._formSelectors.errorClass;
+        const inputErrorClass= this._formSelectors.inputErrorClass;
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+
+        inputElement.classList.remove(inputErrorClass);
+        errorElement.classList.remove(errorClass);
+        errorElement.textContent = '';
+      }
+    
+      _disableSubmitButton(setDisabled) {
+        this._submitButton.disabled = setDisabled;
+        setDisabled ? this._submitButton.classList.add(this._inactiveButtonClass) 
+                    : this._submitButton.classList.remove(this._inactiveButtonClass);
+      }
+}
