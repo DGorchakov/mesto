18165b79c19027ea1018bb8666da1cef45c0a72c
Горчаков Дмитрий <d.gorchakov@hh.ru>
2023-05-20 23:55:55 +0300

    revert
    This reverts commit 9e7b324d55b56834f15bcdf805284e86ae2f4137.

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+export const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]
+
+export const validatorConfig = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit',
+    inactiveButtonClass: 'popup__submit_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  }
+
+ export const editButton = document.querySelector('.profile__edit-button');
+ export const addPlaceButton = document.querySelector('.profile__add-button');
+ export const profileNameSelector = '.profile__title';
+ export const profileAboutSelector = '.profile__about';
+ export const popupWithImageSelector = '.image-popup';
+ export const editProfilePopupSelector = '.profile-popup';
+ export const addPlacePopupSelector = '.place-popup';
+ export const galleryContainerSelector = '.gallery__container';
